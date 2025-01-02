7ade98342c8ef26c55ad544485f41da442b3dd5e
“Furosha” <levent.kyllian@gmail.com>
2025-01-02 13:27:13 +0100

    install nodemon + app.get

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000 from localhost');
+});
+
