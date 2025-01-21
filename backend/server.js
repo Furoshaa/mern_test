4cc7e6dc63e768c1fb8c22004f11b93130dd1e56
“Furosha” <levent.kyllian@gmail.com>
2025-01-21 11:30:03 +0100

    Implement product creation endpoint with validation and error handling

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,22 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
+app.get('/products', async (req, res) => {
+    const product = req.body; // User will send this data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: 'Please fill all fields' });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error("Error in Create product : ", error.message);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
 });
 
 app.listen(5000, () => {
